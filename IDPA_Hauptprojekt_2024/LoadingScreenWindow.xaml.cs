9f5dd856cf98f2f71d6fd2e93f5eb9be45e11cdb
Pascal Martin Oestrich <P.Oestrich.inf22@stud.bbbaden.ch>
2025-01-02 21:00:44 +0100

    Laading screen implemented

---
+++ IDPA_Hauptprojekt_2024/LoadingScreenWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+
+namespace IDPA_Hauptprojekt_2024
+{
+    public partial class LoadingScreenWindow : Window
+    {
+        public LoadingScreenWindow()
+        {
+            InitializeComponent();
+            this.ContentRendered += Window_ContentRendered;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(80); // Simulate work
+            }
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (progressBar.Value == 100)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
