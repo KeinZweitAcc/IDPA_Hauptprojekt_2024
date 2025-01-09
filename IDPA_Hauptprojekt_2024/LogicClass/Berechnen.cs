f1a63c65c8b58d95ca784f54243b5938efc0a714
m.greub.inf22 <M.Greub.inf22@stud.bbbaden.ch>
2025-01-09 14:44:53 +0100

    Ein Kündigungsdatum vor dem Eintrittsdatum ist nicht länger möglich

--- IDPA_Hauptprojekt_2024/LogicClass/Berechnen.cs
+++ IDPA_Hauptprojekt_2024/LogicClass/Berechnen.cs
@@ -8,18 +8,20 @@ namespace IDPA_Hauptprojekt_2024.LogicClass
         {
             TimeSpan? unterschied = kuendigungsdatum - eintrittsdatum;
 
-            if (unterschied == null) return "Kuendigungsfrist nicht berechenbar";
+            if (unterschied == null) return "Kündigungsfrist nicht berechenbar";
+            if (kuendigungsdatum.Value < eintrittsdatum.Value) return "Das Kündigungsdatum kann nicht vor dem Eintrittsdatum liegen.";
+
 
             switch (unterschied?.Days)
             {
                 case <= 365:
-                    return "1 Monat";
+                    return "Kündigungsfrist beträgt 1 Monat";
                 case > 365 and < 3285:
-                    return "2 Monate";
+                    return "Kündigungsfrist beträgt 2 Monate";
                 case > 3285:
-                    return "3 Monate";
+                    return "Kündigungsfrist beträgt 3 Monate";
                 default:
-                    return "Kuendigungsfrist nicht berechenbar";
+                    return "Kündigungsfrist nicht berechenbar";
             }
         }
 
@@ -49,13 +51,13 @@ namespace IDPA_Hauptprojekt_2024.LogicClass
         {
             return days switch
             {
-                <= 365 => "3 Wochen",
-                > 365 and <= 730 => "1 Monat",
-                > 730 and <= 1460 => "2 Monate",
-                > 1460 and <= 3285 => "3 Monate",
-                > 3285 and <= 5110 => "4 Monate",
-                > 5110 and <= 6935 => "5 Monate",
-                > 6935 => "6 Monate",
+                <= 365 => "Lohnfortzahlung beträgt 3 Wochen",
+                > 365 and <= 730 => "Lohnfortzahlung beträgt 1 Monat",
+                > 730 and <= 1460 => "Lohnfortzahlung beträgt 2 Monate",
+                > 1460 and <= 3285 => "Lohnfortzahlung beträgt 3 Monate",
+                > 3285 and <= 5110 => "Lohnfortzahlung beträgt 4 Monate",
+                > 5110 and <= 6935 => "Lohnfortzahlung beträgt 5 Monate",
+                > 6935 => "Lohnfortzahlung beträgt 6 Monate",
                 
             };
         }
@@ -64,10 +66,10 @@ namespace IDPA_Hauptprojekt_2024.LogicClass
         {
             return days switch
             {
-                <= 365 => "3 Wochen",
-                > 365 and <= 1095 => "2 Monate",
-                > 1095 and <= 3650 => "3 Monate",
-                > 3650 => "4 Monate",
+                <= 365 => "Lohnfortzahlung beträgt 3 Wochen",
+                > 365 and <= 1095 => "Lohnfortzahlung beträgt 2 Monate",
+                > 1095 and <= 3650 => "Lohnfortzahlung beträgt 3 Monate",
+                > 3650 => "Lohnfortzahlung beträgt 4 Monate",
                
             };
         }
@@ -76,17 +78,17 @@ namespace IDPA_Hauptprojekt_2024.LogicClass
         {
             return days switch
             {
-                <= 365 => "3 Wochen",
-                > 365 and <= 730 => "8 Wochen",
-                > 730 and <= 1095 => "9 Wochen",
-                > 1095 and <= 1460 => "10 Wochen",
-                > 1460 and <= 1825 => "11 Wochen",
-                > 1825 and <= 2190 => "12 Wochen",
-                > 2190 and <= 2555 => "13 Wochen",
-                > 2555 and <= 2920 => "14 Wochen",
-                > 2920 and <= 3285 => "15 Wochen",
-                > 3285 and <= 3650 => "16 Wochen",
-                > 3650 and <= 4015 => "17 Wochen",
+                <= 365 => "Lohnfortzahlung beträgt 3 Wochen",
+                > 365 and <= 730 => "Lohnfortzahlung beträgt 8 Wochen",
+                > 730 and <= 1095 => "Lohnfortzahlung beträgt 9 Wochen",
+                > 1095 and <= 1460 => "Lohnfortzahlung beträgt 10 Wochen",
+                > 1460 and <= 1825 => "Lohnfortzahlung beträgt 11 Wochen",
+                > 1825 and <= 2190 => "Lohnfortzahlung beträgt 12 Wochen",
+                > 2190 and <= 2555 => "Lohnfortzahlung beträgt 13 Wochen",
+                > 2555 and <= 2920 => "Lohnfortzahlung beträgt 14 Wochen",
+                > 2920 and <= 3285 => "Lohnfortzahlung beträgt 15 Wochen",
+                > 3285 and <= 3650 => "Lohnfortzahlung beträgt 16 Wochen",
+                > 3650 and <= 4015 => "Lohnfortzahlung beträgt 17 Wochen",
                 _ => "Lohnfortzahlung nicht berechenbar"
             };
         }
